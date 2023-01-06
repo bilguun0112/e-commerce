0cf9a8455bd9c9aedac1dfd79be127e67cc57751
ruzishdee <bilguunbayanmunkh10@gmail.com>
2023-01-06 16:03:49 +0800

    mini update

--- e-comm-boot/src/components/catalogi.jsx
+++ e-comm-boot/src/components/catalogi.jsx
@@ -3,24 +3,36 @@ import AliceCarousel from 'react-alice-carousel'
 
 export default function Catalogies() {
 
-     const slides = products.map(dev => {
+    const slides = products.map(dev => {
         return (
-            <div className="container rounded d-flex gap-5 align-items-center border border-1 p-3">
-                <img src={dev.img} alt="" style={{ width: "154px", height: "94px" }} className="p-3" />
-                <div>
-                    <h5>{dev.title}</h5>
-                    <p> ({dev.items.length} items) </p>
+            <div className="d-flex justify-content-evenly align-items-center" >
+                <div className='d-flex justify-content-between h-75 align-items-center border rounded p-3' style={{width: "338px"}}>
+                    <img src={dev.img} alt="" style={{ width: "154px", height: "94px" }} className="p-3" />
+                    <div>
+                        <h5>{dev.title}</h5>
+                        <p> ({dev.items.length} items) </p>
+                    </div>
+
                 </div>
             </div>
+            // <div className="container rounded d-flex gap-3 align-items-center justify-content-evenly border border-1 p-3">
+            //     <img src={dev.img} alt="" style={{ width: "154px", height: "94px" }} className="p-3" />
+            //     <div>
+            //         <h5>{dev.title}</h5>
+            //         <p> ({dev.items.length} items) </p>
+            //     </div>
+            // </div>
 
         )
     })
 
 
-
     return (
-        <AliceCarousel autoPlayInterval="3000" autoPlay="true" responsive={{0: { items: 3 , itemsFit: 'contain'} }}>
-            {slides}
-        </AliceCarousel>
+        <>
+
+            <AliceCarousel autoPlayInterval="3000" autoPlay="true" responsive={{ 0: { items: 3, itemsFit: 'contain' } }}    >
+                {slides}
+            </AliceCarousel>
+        </>
     )
 }
