13a551ef9692c08a1c9d4928ec13100ba9d1430d
Morning <morning@mstars-lab2-10s-iMac.local>
2023-01-10 12:32:56 +0800

    e-commerce update

--- e-comm-boot/src/components/frame53.jsx
+++ e-comm-boot/src/components/frame53.jsx
@@ -1,13 +1,11 @@
 import carData from "../Data/carData"
 import HalfRating from "./ratingStar"
 export default function Frame53() {
-    function myFrame(number) {
-
-
+    function myFrame2(number) {
         return (
-            <div className="container m-2 h-100 border rounded">
+            <div className="container m-2 h-100 border rounded ">
                 <div className="d-flex justify-content-evenly ">
-                    <div className="w-50">
+                    <div className="w-50 p-2">
                         <img className="img-fluid" src={carData[number].img} alt="" />
                     </div>
                     <div className="d-flex flex-column justify-content-center align-items-start">
@@ -20,9 +18,37 @@ export default function Frame53() {
                         <div>
                             <HalfRating />
                         </div>
-                        <div style={{ backgroundColor: "#87BCD9", color: "#272727" ,padding: "8px 14px"}} className="rounded">
+                        <div style={{ backgroundColor: "#87BCD9", color: "#272727", padding: "8px 14px" }} className="rounded d-flex gap-3 align-items-center">
+                            <div>
+                                Add to Card
+                            </div>
+                            <button className="btn rounded-circle" style={{ backgroundColor: "#EDA415" }} ><img src="images/shopping-cart.png" alt="" /></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    function myFrame1(number) {
+        return (
+            <div className="container m-2 h-100 border rounded">
+                <div className="d-flex justify-content-evenly p-3">
+                    <div className="w-50">
+                        <img className="img-fluid" style={{ height: "474px"}} src={carData[number].img} alt="" />
+                    </div>
+                    <div className="d-flex flex-column justify-content-center align-items-start">
+                        <div>
+                            {carData[number].itemName}
+                        </div>
+                        <div>
+                            {carData[number].price}
+                        </div>
+                        <div>
+                            <HalfRating />
+                        </div>
+                        <div style={{ backgroundColor: "#87BCD9", color: "#272727", padding: "8px 14px" }} className="rounded">
                             Add to Card
-                            <button className="btn rounded-circle" style={{ backgroundColor: "#EDA415"}} ><img src="images/shopping-cart.png" alt="" /></button>
+                            <button className="btn rounded-circle" style={{ backgroundColor: "#EDA415" }} ><img src="images/shopping-cart.png" alt="" /></button>
                         </div>
                     </div>
                 </div>
@@ -37,24 +63,24 @@ export default function Frame53() {
         <>
             <style type="text/css">
                 {`
-            .bigCard{
-                height: 500px;
+            .smallCard{
+                height: 250px;
               }
             `}
             </style>
             <div>
-                <div className="row bg-warning">
+                <div className="row">
                     <div className="col-7">
-                       <div className="bg-dark row">
-                       {myFrame(8)}
-                       </div>
+                        <div className="row">
+                            {myFrame1(8)}
+                        </div>
                     </div>
                     <div className="col-5">
-                        <div className="h-50">
-                            {myFrame(20)}
+                        <div className="smallCard">
+                            {myFrame2(20)}
                         </div>
-                        <div className="h-50">
-                            {myFrame(28)}
+                        <div className="smallCard">
+                            {myFrame2(28)}
                         </div>
                     </div>
                 </div>
