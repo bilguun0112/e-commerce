0c6c615f0426584c2b8276f1dbc60ede42f6b625
ruzishdee <bilguunbayanmunkh10@gmail.com>
2023-01-21 11:55:08 +0800

    shopping cart worked

--- e-comm-boot/src/Home.js
+++ e-comm-boot/src/Home.js
@@ -16,11 +16,11 @@ export default function MainHome() {
     return (
         <div className="container">
             <Help />
-            <Search wish={wish} setWish={setWish} />
+            <Search wish={wish} setWish={setWish} cart={cart} />
             <MainMenu />
             <Banner />
             <Catalogies1 />
-            <SubproductCard wish={wish} setWish={setWish} />
+            <SubproductCard wish={wish} setWish={setWish} cart={cart} setCart={setCart} />
             <Sale />
             <Frame53 />
             <QuadFrame />
